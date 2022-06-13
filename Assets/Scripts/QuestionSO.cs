38bb7e1575937fe5695eddce1d9ebac6527d85d1
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-13 12:17:49 +0300

    Add questions and answers to Quiz

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerInsex;
+
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerInsex;
+    }
+
+    public string GetAnswer (int index)
+    {
+        return answers[index];
+    }
+
+}
+
+
+

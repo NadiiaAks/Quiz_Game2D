1951e25409fecd2fb4f302aef4995c0354be8f93
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-14 16:53:50 +0300

    Change sprite image on correct answer buttons

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] GameObject[] answersBtn;
+
+    private int correctAnswerIndex;
+
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questionText.text = question.GetQuestion();
@@ -18,6 +24,26 @@ public class Quiz : MonoBehaviour
             buttonText.text = question.GetAnswer(i);
         }
 
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image btnImage;
+
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            btnImage = answersBtn[index].GetComponent<Image>();
+            btnImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "The correct answer was: \n" + correctAnswer;
+            btnImage = answersBtn[correctAnswerIndex].GetComponent<Image>();
+            btnImage.sprite = correctAnswerSprite;
+        }
     }
 
 }
